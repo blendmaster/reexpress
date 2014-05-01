c694d7322ce1fda04c663fecaa0729a3e0bc7d95
Steven Ruppert <blendmaster@gmail.com>
2014-05-01 17:04:16 -0600

    Document basic approach

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 reexpress
 =========
 
-Performance-driven non-photorealistic face drawing, in javascript
+Performance-driven non-photorealistic face drawing, in javascript, HTML5
+canvas, and SVG.
+
+Authors: Steven Ruppert and Daria Tolmacheva
+
+For Final Project, CSCI 561 Advanced Graphics, Spring 2014, at the Colorado
+School of Mines.
+
+Based on the techniques described in the paper [_Performance-Driven Hand-Drawn
+Animation_][0] by Buck et al.
+
+[0]: http://gfx.cs.princeton.edu/gfx/pubs/Buck_2000_PHA/index.php
--- reexpress.js
+++ reexpress.js
@@ -1,3 +1,46 @@
+// reexpress: performance-driven non-photorealistic face drawing
+// Authors: Steven Ruppert and Daria Tolmacheva
+// For Final Project, CSCI 561 Advanced Graphics, Spring 2014,
+// at the Colorado School of Mines
+//
+// Based on the techniques described in
+// "Performance-Driven Hand-Drawn Animation"
+// http://gfx.cs.princeton.edu/gfx/pubs/Buck_2000_PHA/index.php
 "use strict";
 
-console.log("Well, I don't do too much right now...");
+/*
+Model:
+
+- input control points, representing facial expression:
+  - eyes + eyebrows
+  - lips
+  - head pose is not tracked (i.e. rotation/scale)
+
+- Hand-drawn image templates:
+  - one head model, with holes for eye/eyebrows
+    and lips
+  - 6 lip images, to morph between
+  - 4 eye images, to morph between
+  - Control points for all templates
+
+Model is kept in simple javascript global state, and mutated
+by the view bindings (d3 and `input` events)
+*/
+
+/*
+View:
+
+- `#input` svg, to move input control points
+  - interactions and drawing using d3
+- `#output` canvas
+  - from input control points and templates,
+    template images are warped into the correct facial pose
+    using delaunay triangulation in feature space and
+    [Bieier-Neely warping][0].
+- actual warping/triangulation/drawing algorithms are
+  stateless, and are called by binding functions when
+  input state changes to update view state.
+
+[0]: http://www.hammerhead.com/thad/morph.html
+*/
+
