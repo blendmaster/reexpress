aed2de52466a423f816e1c7fb68475fec0c4ce8e
Steven Ruppert <blendmaster@gmail.com>
2014-05-06 20:33:49 -0600

    Add full control point editor
    Need to do correspondences still, but this
    gets us everything we need, except perhaps
    closed control lines (only for visual clarity);

--- warp.js
+++ warp.js
@@ -35,7 +35,7 @@ var images = [
 
 var $triangle = d3.select('#triangle')
   , $cursor = d3.select('#cursor')
-  , $images = d3.selectAll('.image');
+  , $images = d3.selectAll('#input > .image');
 
 $images.data(points)
   .attr('x', function (d) { return d[0] - 25; })
@@ -44,6 +44,159 @@ $images.data(points)
   .attr('height', 50);
 
 $triangle.attr('d', d3.svg.line()(points) + 'Z');
+
+// for each image, we want to edit control lines on
+// top of them for parameterizing the warp.
+// goals:
+// - drag points to edit
+// - show correspondence to other points
+// - show lines between points (open)
+// - edit control lines
+//  - but this is complicated, how to split them?
+//  - could do individual lines (not multipoint path)
+//    - drag for new line
+//    - click one line for new point
+//    - click on point to delete
+//      - if only two points, delete line
+//
+// Point :: (Double, Double)
+// Path :: [Point]
+// [Path] -> String -> callback -> ()
+// Binds dragging/adding/deleting lines on svg element
+// to the `lines` reference. Calls `callback` when stuff
+// changes.
+//
+// This is only a one-way binding (DOM -> JS).
+var WIDTH = 250, HEIGHT = 250;
+var line = d3.svg.line();
+function bindEditor(paths, svgId, callback) {
+  var svg = d3.select(svgId);
+
+  var dragging = false; // whether a new line is being added;
+  var start, end; // points for new line to add;
+
+  var pending = svg.append('path')
+    .attr('class', 'pending')
+    .style('display', 'none');
+
+  // bind add line behavior
+  svg.call(d3.behavior.drag()
+    .on('drag', function () {
+      var pos = d3.mouse(svg[0][0]);
+      if (start == null) {
+        start = pos;
+      } else {
+        end = pos;
+        pending.style('display', null);
+        drawPending();
+      }
+    })
+    .on('dragstart', function () {
+      start = end = void 0;
+    })
+    .on('dragend', function () {
+      // if start/end line is long enough, add to paths array
+      if (start != null && end != null
+          && dist(start, end) > 10) {
+
+        paths.push([start, end]);
+        bind();
+        callback();
+      }
+      pending.style('display', 'none');
+    }));
+
+  function drawPending() {
+    if (start == null || end == null) return;
+    pending.attr('d', line([start, end]));
+  }
+
+  function bind() {
+    var s = svg.selectAll('.control').data(paths);
+    s.exit().remove();
+    s.enter().append('g')
+      .attr('class', 'control')
+      .append('path').attr('class', 'path')
+      .on('click', function (d) { // add point to line
+        var pos = d3.mouse(svg[0][0]);
+        // find where we are in line
+
+        for (var i = 0, len = d.length - 1; i < len; ++i) {
+          var p1 = d[i];
+          var p2 = d[i+1];
+
+          var p1a = Math.atan2(pos[1] - p1[1], pos[0] - p1[0]);
+          var p2a = Math.atan2(pos[1] - p2[1], pos[0] - p2[0]);
+          if (p1a < 0) p1a += Math.PI * 2;
+          if (p2a < 0) p2a += Math.PI * 2;
+          if (p1a > p2a) p1a -= Math.PI; else p2a -= Math.PI;
+
+          if (Math.abs(p2a - p1a) < 0.5) {
+            // pos is on p1/p2 line
+            d.splice(i + 1, 0, pos);
+
+            bind();
+            callback();
+            return;
+          }
+        }
+      })
+    s.select('path').attr('d', line);
+
+    var p = s.selectAll('.point').data(function (d) { return d });
+    p.exit().remove();
+    p.enter().append('circle')
+      .attr('class', 'point')
+      .attr('r', 15)
+      .call(pointDrag);
+    p.attr('cx', function (d) { return d[0] });
+    p.attr('cy', function (d) { return d[1] });
+  }
+
+  var pointDrag = d3.behavior.drag()
+    .origin(function (d) {
+      return {x: d[0], y: d[1]};
+    })
+    .on('dragstart', function () {
+      d3.event.sourceEvent.stopPropagation();
+    })
+    .on('dragend', function (d, i, j) {
+      // if point is outside image, remove it
+      if (d[0] < 0 || d[0] > WIDTH || d[1] < 0 || d[1] > HEIGHT) {
+        var path = paths[j];
+        path.splice(i, 1);
+
+        // if path doesn't have 2 points, remove
+        if (path.length < 2) {
+          paths.splice(j, 1);
+        }
+        bind();
+        callback();
+      }
+    })
+    .on('drag', function (d, i, j) { // move point
+      d[0] = d3.event.x;
+      d[1] = d3.event.y;
+      bind();
+      callback();
+    })
+
+  bind();
+}
+
+var controls = [
+  [[[80, 125], [160, 125]]],
+  [[[80, 125], [160, 125]]],
+  [[[80, 125], [160, 125]]],
+];
+
+bindEditor(controls[0], '#p0', draw);
+bindEditor(controls[1], '#p1', draw);
+bindEditor(controls[2], '#p2', draw);
+
+// in the callback, the selected line/nodes are highlighted
+// across all three images, and the nodes are adjusted so all
+// three images have the same number of control lines
 
 // ((x1, y1), (x2, y2), (x3, y3)) -> (x, y) -> (u, v, w)
 // w is technically redundant, since u + v + w = 1
@@ -87,8 +240,8 @@ inputs.forEach(function (input, i) {
     var img = images[i];
     img.src = url;
 
-    var simg = d3.select('#si' + i);
-    simg.attr('xlink:href', url);
+    d3.select('#si' + i).attr('xlink:href', url);
+    d3.select('#pi' + i).attr('xlink:href', url);
 
     img.onload = function () {
       window.URL.revokeObjectURL(img.src); // don't need it anymore
@@ -118,3 +271,7 @@ $cursor.call(drag);
 
 $cursor.attr('cx', cursor[0]).attr('cy', cursor[1]);
 draw();
+
+function dist(a, b) {
+  return Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
+}
